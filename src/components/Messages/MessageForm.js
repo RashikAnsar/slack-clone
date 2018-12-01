fe831dca62655f088fc0bc7d78aa8ca0332914b2
RashikAnsar <sk.ansar46@gmail.com>
2018-12-01 11:45:41 +0530

    Added progress bar file upload

--- src/components/Messages/MessageForm.js
+++ src/components/Messages/MessageForm.js
@@ -4,6 +4,7 @@ import firebase from '../../firebase';
 import { Segment, Button, Input } from 'semantic-ui-react';
 
 import FileModal from './FileModal';
+import ProgressBar from './ProgressBar';
 
 class MessageForm extends React.Component {
   state = {
@@ -88,6 +89,7 @@ class MessageForm extends React.Component {
             const percentUploaded = Math.round(
               (snap.bytesTransferred / snap.totalBytes) * 100
             );
+            this.props.isProgressBarVisible(percentUploaded);
             this.setState({ percentUploaded });
           },
           err => {
@@ -135,7 +137,14 @@ class MessageForm extends React.Component {
   };
 
   render() {
-    const { errors, message, loading, modal } = this.state;
+    const {
+      errors,
+      message,
+      loading,
+      modal,
+      uploadState,
+      percentUploaded
+    } = this.state;
 
     return (
       <Segment className="message__form">
@@ -165,17 +174,22 @@ class MessageForm extends React.Component {
           />
           <Button
             color="teal"
+            disabled={uploadState === 'uploading'}
             onClick={this.openModal}
             content="Upload Media"
             labelPosition="right"
             icon="cloud upload"
           />
-          <FileModal
-            modal={modal}
-            closeModal={this.closeModal}
-            uploadFile={this.uploadFile}
-          />
         </Button.Group>
+        <FileModal
+          modal={modal}
+          closeModal={this.closeModal}
+          uploadFile={this.uploadFile}
+        />
+        <ProgressBar
+          uploadState={uploadState}
+          percentUploaded={percentUploaded}
+        />
       </Segment>
     );
   }
--- src/components/Messages/Messages.js
+++ src/components/Messages/Messages.js
@@ -12,7 +12,8 @@ class Messages extends React.Component {
     messages: [],
     messagesLoading: true,
     channel: this.props.currentChannel,
-    user: this.props.currentUser
+    user: this.props.currentUser,
+    progressBar: false
   };
 
   componentDidMount() {
@@ -48,13 +49,21 @@ class Messages extends React.Component {
       />
     ));
 
+  isProgressBarVisible = percent => {
+    if (percent > 0) {
+      this.setState({ progressBar: true });
+    }
+  };
+
   render() {
-    const { messagesRef, channel, messages, user } = this.state;
+    const { messagesRef, channel, messages, user, progressBar } = this.state;
     return (
       <React.Fragment>
         <MessagesHeader />
         <Segment>
-          <Comment.Group className="messages">
+          <Comment.Group
+            className={progressBar ? 'messages__progress' : 'messages'}
+          >
             {this.displayMessages(messages)}
           </Comment.Group>
         </Segment>
@@ -62,6 +71,7 @@ class Messages extends React.Component {
           messagesRef={messagesRef}
           currentChannel={channel}
           currentUser={user}
+          isProgressBarVisible={this.isProgressBarVisible}
         />
       </React.Fragment>
     );

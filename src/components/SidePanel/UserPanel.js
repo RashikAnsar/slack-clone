08c752268fd5fa197cd11916b4bb3f07e6b4d132
RashikAnsar <sk.ansar46@gmail.com>
2018-12-02 16:49:17 +0530

    Color panel

--- src/components/SidePanel/UserPanel.js
+++ src/components/SidePanel/UserPanel.js
@@ -37,8 +37,10 @@ class UserPanel extends React.Component {
 
   render() {
     const { user } = this.state;
+    const { primaryColor } = this.props;
+
     return (
-      <Grid style={{ background: '#4C3C4C' }}>
+      <Grid style={{ background: primaryColor }}>
         <Grid.Column>
           <Grid.Row style={{ padding: '1.2em', margin: 0 }}>
             <Header inverted floated="left" as="h2">
